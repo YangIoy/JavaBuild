20bacde9ff326fd92d1583c230efb299364bd001
YangIoy <121197143@qq.com>
2023-08-17 21:20:36 +0800

    msd

--- src/Text.java
+++ src/Text.java
@@ -1,6 +1,29 @@
 
 public class Text {
     public static void main(String[] args) {
+        fun(new fang());
+        fun(new tuo());
+    }
 
+    private static void fun(tu a) {
+        a.hua();
+    }
+
+}
+class tu{
+    public void hua(){
+        System.out.println("画");
+    }
+}
+class fang extends tu{
+    @Override
+    public void hua() {
+        System.out.println("画方");
+    }
+}
+class tuo extends tu{
+    @Override
+    public void hua() {
+        System.out.println("画椭");
     }
 }
