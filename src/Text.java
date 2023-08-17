a4a9a0d9f518f6536c307889151541db17734c06
YangIoy <121197143@qq.com>
2023-08-17 21:05:19 +0800

    msd

--- src/Text.java
+++ src/Text.java
@@ -1,16 +1,6 @@
-import Box1.*;
 
-import static Box1.Fun.fun;
-
-/**
- * @program: Item
- * @description: Text
- * @author: Mr.IoyYang
- * @create: 2023-08-12 21:16
- **/
 public class Text {
     public static void main(String[] args) {
-        fun(new Cat());
-        fun(new Dog());
+
     }
 }
