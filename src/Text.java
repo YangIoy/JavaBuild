f71c1506852efea2840ca29a71bb364c854b7f19
YangIoy <121197143@qq.com>
2023-08-17 16:19:34 +0800

    msd

---
+++ src/Text.java
@@ -0,0 +1,17 @@
+import Box1.*;
+
+/**
+ * @program: Item
+ * @description: Text
+ * @author: Mr.IoyYang
+ * @create: 2023-08-12 21:16
+ **/
+public class Text {
+    public static void main(String[] args) {
+        Box1.Student person = new Box1.Student(18,"Yang");
+        person.setName("LianGE");
+        System.out.println(person);
+        Box1.Student2 people = new Student2(15,"ZiFeng");
+        System.out.println(people);
+    }
+}
